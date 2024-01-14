ed6f083b3263068264629f0fbc2aafeed9e96102
Nathan <na.than.fjv@gmail.com>
2024-01-14 18:35:26 -0300

    style: add position relative

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -4,7 +4,7 @@ interface TitleProps {
 
 export function Title({ title }: TitleProps) {
   return (
-    <div className="after:bg-gradient-contact after:h[1px] before:-z-1 before:bg-gradient-profile z-[2] min-h-[50px] pb-6 text-2xl font-semibold text-gray-100 before:absolute before:-left-[10px] before:top-0 before:h-[30px] before:w-[30px] before:rounded-[30px] after:absolute after:-left-[30px] after:bottom-0 after:right-0 after:h-[1px]">
+    <div className="before:-z-1 relative z-[2] min-h-[50px] pb-6 text-2xl font-semibold text-gray-100 before:absolute before:-left-[10px] before:top-0 before:h-[30px] before:w-[30px] before:rounded-[30px] before:bg-gradient-profile after:absolute after:-left-[30px] after:bottom-0 after:right-0 after:h-[1px] after:bg-gradient-contact">
       <h1 className="first-letter:text-orange">{title}</h1>
     </div>
   )
