9c4de49c703e63d61eccfafdcc8e7fd90b1efc84
Nathan <na.than.fjv@gmail.com>
2024-01-16 13:43:30 -0300

    feat: data to skill component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
 import { About } from '@/components/About'
 import { ContentWrapper } from '@/components/ContentWrapper'
+import { Skill } from '@/components/Skill'
+import { fetchAPI } from '@/lib/api'
 
-export default function Home() {
+type Hero = {
+  title: string
+  about: string
+}
+
+type Tech = {
+  id: number
+  title: string
+  icon: {
+    url: string
+  }
+}
+
+type HomePageData = {
+  hero: Hero
+  Techs: Tech[]
+}
+
+export default async function Home() {
+  const data: HomePageData = await fetchAPI('/homepage')
+
   return (
     <ContentWrapper title="About">
-      <About />
+      <About title={data.hero.title} about={data.hero.about} />
+      <Skill techs={data.Techs} />
     </ContentWrapper>
   )
 }
